8d5c78a219c0cef53875cccfaf4132cc7db7ec2e
RogerKlock <rogerklock08@gmail.com>
2024-05-20 21:49:32 -0300

    Card

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import Card from './components/Card'
+import Imag from './assets/images/Imagem do produto.png'
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Card title="João pedro" price="R$20,00" imgSrc={Imag} description="O mais viadão de todos"></Card>
     </>
   );
 }
