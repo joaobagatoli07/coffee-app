f487110734f30b3c233dc8891e3cf0fe43bae40f
joaopedrowalter <joaowalter0509@gmail.com>
2024-05-22 22:00:43 -0300

    Tela de Feedback

---
+++ src/pages/Feedback.jsx
@@ -0,0 +1,15 @@
+import MainButton from '../components/MainButton.jsx'
+
+function Feedback({imagem}) {
+    return (
+        <div className="flex justify-center items-center h-screen">
+            <div className="flex flex-col justify-center items-center gap-10">
+                <img src={imagem} />
+                <p className="font-rosarivo text-2xl text-gray-600 text-center">Conta desconectada <br />com sucesso</p>
+                <MainButton text={'Retornar a tela inicial'} bgColor={'bg-amber-950'}></MainButton>
+            </div>
+        </div>
+    )
+}
+
+export default Feedback
