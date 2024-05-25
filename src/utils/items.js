22d0c3880d62780d52c9abbf705fac250c2c6604
João Bagatoli <joaovitorbagatoli07@gmail.com>
2024-05-25 15:00:41 -0300

    Criação de array de objetos com informações do café e melhoria na organização de pastas

---
+++ src/utils/items.js
@@ -0,0 +1,13 @@
+const items = [
+  {
+    id: 1,
+    title: 'Caramel Macchiato',
+    subtitle: 'Rico espresso com um toque doce de caramelo',
+    description: 'Uma combinação perfeita de rico espresso, leite vaporizado e xarope de caramelo, coberto com um fio de calda de caramelo. Ideal para uma experiência de café doce e cremosa.',
+    prices: {
+        small: 3.50,
+        medium: 4.50,
+        large: 5.50
+    }
+  },
+];
