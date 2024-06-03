6667a25612084f8a0f20ea9cab418b503119a762
Emilly Back <back.emilly@gmail.com>
2024-06-03 20:54:09 -0300

    [Dados] Popular array de objetos que contém informações sobre os cafés

--- src/utils/items.js
+++ src/utils/items.js
@@ -10,4 +10,158 @@ const items = [
         large: 5.50
     }
   },
+  {
+    id: 2,
+    title: 'Espresso',
+    subtitle: 'O clássico dos clássicos!',
+    description: 'Um shot de café concentrado que desperta seus sentidos. Experimente-o na sua forma mais pura.',
+    prices: {
+        small: 3.50,
+        medium: 4.50,
+        large: 6.50
+    }
+  },
+  {
+    id: 3,
+    title: 'Café Americano',
+    subtitle: 'Uma explosão de sabor americano!',
+    description: 'Nosso espresso suave e robusto misturado com água quente para criar uma bebida reconfortante e revigorante.',
+    prices: {
+        small: 4.50,
+        medium: 5.50,
+        large: 7.50
+    }
+  },
+  {
+    id: 4,
+    title: 'Café Latte',
+    subtitle: 'Um abraço em uma xícara!',
+    description: 'Espresso cremoso combinado com leite vaporizado, resultando em uma experiência macia e reconfortante.',
+    prices: {
+        small: 4.50,
+        medium: 5.90,
+        large: 7.50
+    }
+  },
+  {
+    id: 5,
+    title: 'Cappuccino',
+    subtitle: 'O equilíbrio perfeito de café, leite e espuma!',
+    description: 'Nosso espresso encorpado misturado com leite vaporizado e uma generosa camada de espuma cremosa.',
+    prices: {
+        small: 5.50,
+        medium: 7.60,
+        large: 8.50
+    }
+  },
+  {
+    id: 6,
+    title: 'Macchiato',
+    subtitle: 'Uma explosão de sabor em cada gole!',
+    description: 'Nosso espresso marcante com um toque sutil de leite vaporizado para adoçar sua jornada.',
+    prices: {
+        small: 7.50,
+        medium: 9.50,
+        large: 10.90
+    }
+  },
+  {
+    id: 7,
+    title: 'Mocha',
+    subtitle: 'Uma indulgência irresistível!',
+    description: 'Espresso rico e chocolate cremoso combinados com leite vaporizado e cobertos com uma nuvem de chantilly.',
+    prices: {
+        small: 11.99,
+        medium: 15.50,
+        large: 17.50
+    }
+  },
+  {
+    id: 8,
+    title: 'Affogato',
+    subtitle: 'Uma sobremesa e uma bebida em uma só!',
+    description: 'Sorvete de baunilha luxuoso afogado em um shot de espresso quente. Uma explosão de prazer!',
+    prices: {
+        small: 15.50,
+        medium: 18.50,
+        large: 20.50
+    }
+  },
+  {
+    id: 9,
+    title: 'Café Frappé',
+    subtitle: 'Uma explosão de frescor e sabor!',
+    description: 'Café gelado batido com gelo, leite e seu xarope favorito, proporcionando uma delícia refrescante para qualquer ocasião.',
+    prices: {
+        small: 11.50,
+        medium: 15.50,
+        large: 17.50
+    }
+  },
+  {
+    id: 10,
+    title: 'Flat White',
+    subtitle: 'Uma experiência suave e sedosa!',
+    description: 'Espresso encorpado misturado com leite vaporizado, sem a espuma, para um acabamento aveludado.',
+    prices: {
+        small: 9.99,
+        medium: 12.50,
+        large: 16.50
+    }
+  },
+  {
+    id: 11,
+    title: 'Café Viennese',
+    subtitle: 'Uma indulgência clássica com um toque especial!',
+    description: 'Espresso suave coberto com chantilly sedoso e um toque de cacau, uma verdadeira obra-prima',
+    prices: {
+        small: 9.50,
+        medium: 13.50,
+        large: 15.99
+    }
+  },
+  {
+    id: 12,
+    title: 'Café Turco',
+    subtitle: 'RUma viagem sensorial única!co espramelo',
+    description: 'Café forte e aromático, preparado de maneira tradicional, garantindo uma experiência intensa e memorável.',
+    prices: {
+        small: 4.60,
+        medium: 5.50,
+        large: 6.50
+    }
+  },
+  {
+    id: 13,
+    title: 'Café com Rum',
+    subtitle: 'Um deleite para os sentidos!',
+    description: 'Espresso aromático com uma pitada de rum e uma generosa cobertura de chantilly. Uma verdadeira indulgência!',
+    prices: {
+        small: 6.50,
+        medium: 8.50,
+        large: 10.50
+    }
+  },
+  {
+    id: 14,
+    title: 'Café Irlandês',
+    subtitle: 'Uma explosão de sabores!',
+    description: 'Café quente misturado com o caloroso sabor do uísque irlandês, coberto com chantilly e um toque de açúcar mascavo. Uma experiência para ser saboreada.',
+    prices: {
+        small: 10.50,
+        medium: 13.50,
+        large: 16.50
+    }
+  },
+  {
+    id: 15,
+    title: 'Café Gelado',
+    subtitle: 'O refresco perfeito para qualquer dia!',
+    description: 'Café gelado, adoçado na medida certa e servido com cubos de gelo para uma experiência revigorante.',
+    prices: {
+        small: 6.50,
+        medium: 8.50,
+        large: 12.50
+    }
+  },
 ];
