c61f51540a084610b11978f552728d95a56e8cd4
João Vitor Bagatoli <joaoviorbagatoli07@gmail.com>
2024-06-03 21:21:34 -0300

    Nav: configuração do roteamento da SPA

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Nav from './components/layout/Nav.jsx'
+import Home from './pages/Home.jsx'
+import Cart from './pages/Cart.jsx'
+import Orders from './pages/Orders.jsx'
 
 function App() {
   return (
-    <>
-      
-    </>
+    <Router>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/Carrinho" element={<Cart />}></Route>
+        <Route path="/Pedidos" element={<Orders />}></Route>
+      </Routes>
+    </Router>
   );
 }
 
