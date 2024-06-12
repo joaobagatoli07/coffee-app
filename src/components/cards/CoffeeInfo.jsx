db197fd800cebf98a6a6b5249c4589f50e024c6a
Emilly Back <back.emilly@gmail.com>
2024-06-12 19:57:22 -0300

    ajuste da definição da imagens dos produtos

--- src/components/cards/CoffeeInfo.jsx
+++ src/components/cards/CoffeeInfo.jsx
@@ -4,7 +4,7 @@ function CoffeeInfo({ imgSrc, title, subtitle, price }) {
   return (
       <div className="h-52 w-40 shadow-lg rounded-lg border border-solid border-slate-300">
         <div className="h-2/4">
-          <img src={imgSrc} className="h-full w-full rounded" alt={title}  />
+          <img src={imgSrc} className="h-full object-cover w-full rounded " alt={title}  />
         </div>
         <div className="h-2/4 p-2">
           <div className="h-4/6">
