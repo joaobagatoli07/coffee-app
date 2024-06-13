6b036e09e7d3f7165a3040c59da3beb93f9d34f2
joaopedrowalter <joaowalter0509@gmail.com>
2024-06-12 21:28:29 -0300

    Botão intemediario

---
+++ src/components/buttons/IntermediateButton.jsx
@@ -0,0 +1,7 @@
+function IntermediateButton({ text }) {
+    return (
+        <button className="p-1 text-sm w-40 rounded-md bg-amber-800 text-white ">{text}</button>
+    );
+}
+
+export default IntermediateButton
