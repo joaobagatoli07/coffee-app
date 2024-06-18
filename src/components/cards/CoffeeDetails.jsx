5cb261c014d426f51777d995240b1e5e51fc10f3
Emilly Back <back.emilly@gmail.com>
2024-06-17 21:26:21 -0300

    Coffee_Details_inicio

--- src/components/cards/CoffeeDetails.jsx
+++ src/components/cards/CoffeeDetails.jsx
@@ -1,7 +1,16 @@
-function CoffeeDetails() { 
-    return (
-       <h1>Coffee Details</h1>
-    )
-}       
 
-export default CoffeeDetails 
+function CoffeeDetails({ imgSrc, title, subtitle, description }) {
+
+    return (
+        <div class="flex h-52 w-96 shadow-lg rounded-lg border border-solid border-slate-300">
+            <div class="flex justify-center w-2/5" >
+                <img src={imgSrc} className="flex h-32 w-62 justify-center mt-6 rounded-lg" alt={title} />
+            </div>
+            <div className="w-3/5 ">
+
+            </div> 
+        </div>
+    );
+}
+
+export default CoffeeDetails
--- src/routes/Coffee.jsx
+++ src/routes/Coffee.jsx
@@ -1,11 +1,17 @@
 import { useLocation } from "react-router-dom"
+import CoffeeDetails from "../components/cards/CoffeeDetails";
 
 function Coffee(){
     const location = useLocation();
     const { imgSrc, title, subtitle, price } = location.state || {};
 
     return (
+        <>
         <h1>{title}</h1>
+        <CoffeeDetails imgSrc="src/assets/images/coffee/product/Caramel_Macchiato4.png" />
+        </>
+        
+        
     )
 }
 
