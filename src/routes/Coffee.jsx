f52f82c388198e48f7ff25aab9f7bae056c418d3
Emilly Schimanko Back <118355883+Back-Emy@users.noreply.github.com>
2024-06-24 23:37:57 +0000

    teste

--- src/routes/Coffee.jsx
+++ src/routes/Coffee.jsx
@@ -21,7 +21,7 @@ function Coffee() {
                     </section>
                     <section className="flex mt-10 h-96">
                         <div className="flex w-3/5 items-center justify-center ">
-                            <div className="flex w-11/12 h-5/6 rounded-lg border-solid border-slate-300 border-gray-800 shadow-[_10px_10px_15px_rgba(0,0,0,0.1)]">
+                            <div className="flex w-11/12 h-5/6 rounded-lg border-solid border-gray-800 shadow-[_10px_10px_15px_rgba(0,0,0,0.1)]">
 
                             </div>                                                                                              
 
