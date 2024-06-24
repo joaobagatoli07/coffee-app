e58031f41179253ab45122ef5dfb09f1fc57f290
João Vitor Bagatoli <joaoviorbagatoli07@gmail.com>
2024-06-24 19:13:48 -0300

    Correções footer

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -4,20 +4,20 @@ import Whatsapp from '../../assets/icons/Whatsapp'
 import Logo from '../../assets/icons/Logo'
 import { Link } from 'react-router-dom'
 
-function Footer({ resetCoffees }) {
+function Footer() {
     return (
         <footer className='flex justify-center pt-4 pb-4 bg-amber-950 text-white text-sm'>
             <div className='flex justify-between w-4/5'>
                 <div>
-                    <Link to='/' onClick={resetCoffees}><Logo width='35' height='51'></Logo></Link>
+                    <Link to='/'><Logo width='35' height='51'></Logo></Link>
                     <p>Contato: 47 99698-9898</p>
                     <p>Email: coffe@gmail.com</p>
                 </div>
                 <div className='flex flex-col items-end gap-5'>
                     <div className='flex gap-2'>
-                        <a href="https://www.instagram.com/accounts/login/"><Instagram></Instagram></a>
-                        <a href="https://www.facebook.com/?locale=pt_BR"><Facebook></Facebook></a>
-                        <a href="https://www.whatsapp.com/?lang=pt_BR"><Whatsapp></Whatsapp></a>
+                        <a href="https://www.instagram.com/accounts/login/" target="_blank"><Instagram></Instagram></a>
+                        <a href="https://www.facebook.com/?locale=pt_BR" target="_blank"><Facebook></Facebook></a>
+                        <a href="https://www.whatsapp.com/?lang=pt_BR" target="_blank"><Whatsapp></Whatsapp></a>
                     </div>
                     <div className='flex flex-col'>
                         <p className='text-right'>Política de Privacidade / Termos & Condições</p>
