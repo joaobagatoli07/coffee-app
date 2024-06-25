075cb670d3221fbfdc222c5eec5fbcf7bb645baf
joaopedrowalter <joaowalter0509@gmail.com>
2024-06-24 21:40:29 -0300

    tela padrão de erro

--- src/routes/Feedback.jsx
+++ src/routes/Feedback.jsx
@@ -1,10 +1,11 @@
 import MainButton from "../components/buttons/MainButton.jsx";
+import FeedbackImg from "../assets/images/coffee/pages/feedbacktela.png"
 
-function Feedback({ imagem }) {
+function Feedback() {
   return (
     <div className="flex justify-center items-center h-screen">
       <div className="flex flex-col justify-center items-center gap-10">
-        <img src={imagem} />
+        <img src={FeedbackImg} />
         <p className="font-rosarivo text-2xl text-gray-600 text-center">
           Conta desconectada <br />
           com sucesso
