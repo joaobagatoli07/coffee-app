77f0e71ba6b5395794ff672642edd604c6b3f00f
Emilly Schimanko Back <118355883+Back-Emy@users.noreply.github.com>
2024-06-28 00:57:07 +0000

    ajuste-texto_coffee_detalhes

--- src/routes/Coffee.jsx
+++ src/routes/Coffee.jsx
@@ -22,7 +22,7 @@ function Coffee() {
                             <Heart size={30} className="stroke-amber-800" />
                         </div>
                     </section>
-                    <section className="flex mt-10 h-96"> 
+                    <section className="flex mt-10 h-96">
                         <div className="flex w-3/5 items-center justify-center ">
                             <div className="flex w-11/12 h-5/6 rounded-lg border-solid bg-zinc-100 border-gray-800 shadow-[_10px_10px_15px_rgba(0,0,0,0.1)]">
                                 <div className="flex w-2/4 items-center justify-center" >
@@ -30,22 +30,20 @@ function Coffee() {
                                         <img src="" alt="" />
                                     </div>
                                 </div>
-                                <div className="flex w-2/4 items-center justify-center">
-                                    <div className="flex-row w-11/12 h-5/6 rounded-lg">
-                                        <div className="flex-col w-full">
-                                            <div className="font-inter text-2xl text-amber-800">Caramel Macchiato</div>
-                                            <div className="font-inter text-base text-black">Rico espresso com um toque doce de caramelo</div>
-                                            <div className="font-inter text-2xl text-amber-800">Descrição</div>
-                                            <div className="font-inter text-base text-black">Uma combinação perfeita de rico espresso, leite vaporizado e xarope de caramelo, coberto com um fio de calda de caramelo. Ideal para uma experiência de café doce e cremosa.</div>
-                                        </div>
+                                <div className="flex w-2/4 items-center">
+                                    <div className="flex-col w-10/12 justify-center">
+                                        <div className="font-inter text-2xl text-amber-800">Caramel Macchiato</div>
+                                        <div className="font-inter text-base text-black">Rico espresso com um toque doce de caramelo</div>
+                                        <div className="font-inter text-2xl text-amber-800">Descrição</div>
+                                        <div className="font-inter text-base text-black">Uma combinação perfeita de rico espresso, leite vaporizado e xarope de caramelo, coberto com um fio de calda de caramelo. Ideal para uma experiência de café doce e cremosa.</div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                         <div className="flex w-2/5 items-center justify-center">
                             <div className="flex items-center justify-center w-11/12 h-5/6 bg-zinc-100 rounded-lg shadow-[_10px_10px_25px_rgba(0,0,0,0.1)] border-solid border-slate-200">
-                                <CoffeSizes/>
-                                
+                                <CoffeSizes />
+
                             </div>
                         </div>
                     </section>
