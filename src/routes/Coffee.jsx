ac7241d802616b72f68ae09e1040b06005e4ef2f
João Bagatoli <joaovitorbagatoli07@gmail.com>
2024-06-28 00:14:44 -0300

    Tela de café dinâmica

--- src/routes/Coffee.jsx
+++ src/routes/Coffee.jsx
@@ -3,12 +3,10 @@ import { items } from "../data/items";
 import { Heart } from "lucide-react";
 import CoffeSizes from "../components/cards/CoffeeSizes";
 
-//import CoffeePrice from "../components/cards/CoffeePrice";
-
 function Coffee() {
     const location = useLocation();
     const { title } = location.state || {};
-    const coffee = items.find(coffee => coffee.title == title)
+    const coffee = items.find(coffee => coffee.title == title);
 
     return (
         <>
@@ -26,16 +24,16 @@ function Coffee() {
                         <div className="flex w-3/5 items-center justify-center ">
                             <div className="flex w-11/12 h-5/6 rounded-lg border-solid bg-zinc-100 border-gray-800 shadow-[_10px_10px_15px_rgba(0,0,0,0.1)]">
                                 <div className="flex w-2/4 items-center justify-center" >
-                                    <div className="flex w-11/12 h-3/6 rounded-lg bg-green-300">
-                                        <img src="" alt="" />
+                                    <div className="flex w-11/12 h-3/6 rounded-lg">
+                                        <img src={coffee.image} alt={coffee.title} />
                                     </div>
                                 </div>
                                 <div className="flex w-2/4 items-center">
                                     <div className="flex-col w-10/12 justify-center">
-                                        <div className="font-inter text-2xl text-amber-800">Caramel Macchiato</div>
-                                        <div className="font-inter text-base text-black">Rico espresso com um toque doce de caramelo</div>
-                                        <div className="font-inter text-2xl text-amber-800">Descrição</div>
-                                        <div className="font-inter text-base text-black">Uma combinação perfeita de rico espresso, leite vaporizado e xarope de caramelo, coberto com um fio de calda de caramelo. Ideal para uma experiência de café doce e cremosa.</div>
+                                        <div className="text-2xl text-amber-800">{coffee.title}</div>
+                                        <div className="text-base text-black">{coffee.subtitle}</div>
+                                        <div className="text-2xl text-amber-800">Descrição</div>
+                                        <div className="text-base text-black">{coffee.description}</div>
                                     </div>
                                 </div>
                             </div>
@@ -43,7 +41,6 @@ function Coffee() {
                         <div className="flex w-2/5 items-center justify-center">
                             <div className="flex items-center justify-center w-11/12 h-5/6 bg-zinc-100 rounded-lg shadow-[_10px_10px_25px_rgba(0,0,0,0.1)] border-solid border-slate-200">
                                 <CoffeSizes />
-
                             </div>
                         </div>
                     </section>
