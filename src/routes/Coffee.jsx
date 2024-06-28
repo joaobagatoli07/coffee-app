8cd489cd73f49ba728dca245590cd1d2c9de68b5
Emilly Schimanko Back <118355883+Back-Emy@users.noreply.github.com>
2024-06-28 00:44:07 +0000

    Coffee_detalhes_2.0

--- src/routes/Coffee.jsx
+++ src/routes/Coffee.jsx
@@ -13,7 +13,7 @@ function Coffee() {
     return (
         <>
             <div className="h-screen flex justify-center">
-                <main className="flex-col w-4/5">
+                <main className="w-4/5">
                     <section className="flex mt-10">
                         <div className="w-11/12 flex justify-center">
                             <span className="font-rosarivo text-4xl text-amber-800">Detalhes</span>
@@ -22,7 +22,7 @@ function Coffee() {
                             <Heart size={30} className="stroke-amber-800" />
                         </div>
                     </section>
-                    <section className="flex mt-10 h-96 ">
+                    <section className="flex mt-10 h-96"> 
                         <div className="flex w-3/5 items-center justify-center ">
                             <div className="flex w-11/12 h-5/6 rounded-lg border-solid bg-zinc-100 border-gray-800 shadow-[_10px_10px_15px_rgba(0,0,0,0.1)]">
                                 <div className="flex w-2/4 items-center justify-center" >
@@ -32,11 +32,11 @@ function Coffee() {
                                 </div>
                                 <div className="flex w-2/4 items-center justify-center">
                                     <div className="flex-row w-11/12 h-5/6 rounded-lg">
-                                        <div className="flex w-full h-1/4 bg-blue-200">
-
-                                        </div>
-                                        <div className="flex w-full h-3/4 bg-blue-300">
-
+                                        <div className="flex-col w-full">
+                                            <div className="font-inter text-2xl text-amber-800">Caramel Macchiato</div>
+                                            <div className="font-inter text-base text-black">Rico espresso com um toque doce de caramelo</div>
+                                            <div className="font-inter text-2xl text-amber-800">Descrição</div>
+                                            <div className="font-inter text-base text-black">Uma combinação perfeita de rico espresso, leite vaporizado e xarope de caramelo, coberto com um fio de calda de caramelo. Ideal para uma experiência de café doce e cremosa.</div>
                                         </div>
                                     </div>
                                 </div>
